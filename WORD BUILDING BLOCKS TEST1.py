6f197e0bc90fb5fe400dc843561b3cf9ac695b47
etiennely13 <78334313+etiennely13@users.noreply.github.com>
2021-02-01 00:44:30 -0500

    test1 organization changes
    Changed the organization

--- WORD BUILDING BLOCKS TEST1.py
+++ WORD BUILDING BLOCKS TEST1.py
@@ -73,23 +73,23 @@ create_file = open("testament_"+no+".txt", "a")
 create_file.write("TESTAMENT:\n\nDE: "+no+"\nDate: "+da+"\n\nTestament\n\nCeci est le testament du (de la) soussigné(e), "+no+" ,domicilié(e) et résidant au"+adLi1+", province de Quebec, fait ce "+da+".")
 
 if re=="oui":
-    create_file.write("\n\nJE RÉVOQUE tous les testaments, codicilles et dispositions testamentaires que j’ai faits antérieurement au présent testament.")
+    create_file.write("\n\n\nJE RÉVOQUE tous les testaments, codicilles et dispositions testamentaires que j’ai faits antérieurement au présent testament.")
 else:
     pass
 
-create_file.write("\n\n\nJE NOMME "+noLi1+", domicilié et résidant au adresse du liquidateur province de Québec, à titre de liquidateur de ma succession.")
+create_file.write("\n\nJE NOMME "+noLi1+", domicilié et résidant au adresse du liquidateur province de Québec, à titre de liquidateur de ma succession.")
 
 if deLi=="oui":
-    create_file.write("\n\n\nCEPENDANT, si le liquidateur ainsi désigné devait refuser d’agir, décéder avant moi ou décéder dans un délai de "+deLi2+" jours après mon décès, ALORS JE NOMME "+noLi2+", domicilié et résidant au"+adLi2+", province de Québec, pour agir comme remplaçant.")
+    create_file.write("\n\nCEPENDANT, si le liquidateur ainsi désigné devait refuser d’agir, décéder avant moi ou décéder dans un délai de "+deLi2+" jours après mon décès, ALORS JE NOMME "+noLi2+", domicilié et résidant au "+adLi2+", province de Québec, pour agir comme remplaçant.")
 else:
     pass
 
 if poLi=="oui":
-    create_file.write("\n\n\nPOUVOIRS. Le liquidateur est chargé de voir à l’exécution de mon testament et à la liquidation de ma succession. Il est par les présentes investi de tous les pouvoirs requis pour ce faire. Il aura droit au remboursement des dépenses qu’il encourra pour l’exercice de cette charge et à une rémunération globale de"+moPaLi+"$.")
+    create_file.write("\n\nPOUVOIRS. Le liquidateur est chargé de voir à l’exécution de mon testament et à la liquidation de ma succession. Il est par les présentes investi de tous les pouvoirs requis pour ce faire. Il aura droit au remboursement des dépenses qu’il encourra pour l’exercice de cette charge et à une rémunération globale de "+moPaLi+"$.")
 else:
     pass
 
-create_file.write("\n\n\nJE CHARGE mon liquidateur de payer et d’acquitter toutes mes dettes, dépenses funéraires et testamentaires, mes droits ou impôts sur les successions, le cas échéant, et toutes les dépenses normales qui en découlent et ce aussitôt qu’il conviendra de le faire après mon décès.")
+create_file.write("\n\nJE CHARGE mon liquidateur de payer et d’acquitter toutes mes dettes, dépenses funéraires et testamentaires, mes droits ou impôts sur les successions, le cas échéant, et toutes les dépenses normales qui en découlent et ce aussitôt qu’il conviendra de le faire après mon décès.")
 
 if lePa=="oui":
     create_file.write("JE FAIS les legs suivants, à titre particulier :\n\nÀ "+noLe1+", je lègue: "+biLe1+"\n\nÀ "+noLe2+", je lègue: "+biLe2)
@@ -97,7 +97,7 @@ else:
     pass
 
 if leUn=="oui":
-    create_file.write("\n\nJ'INSTITUE "+noLeUn+" légataire universel résiduaire de l’ensemble de mes biens.\n\nOU")
+    create_file.write("\n\nJ'INSTITUE "+noLeUn+" légataire universel résiduaire de l’ensemble de mes biens.\nOU")
 else:
     pass
 
